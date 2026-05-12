dc725ea04da621ca8b183d87906434b4b222c28c
myriam Hamam <155687205+myriam-hamam@users.noreply.github.com>
2026-05-12 04:27:54 +0300

    Delete CashOnDeliveryPayment.java

--- CashOnDeliveryPayment.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.strategy;
-
-public class CashOnDeliveryPayment implements PaymentStrategy {
-    
-    @Override
-    public void pay(double amount) {
-        System.out.println("Payment of $" + amount + " will be collected in cash upon delivery.");
-    }
-}
